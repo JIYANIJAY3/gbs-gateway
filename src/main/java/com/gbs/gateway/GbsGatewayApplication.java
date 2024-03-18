d96d2e7cbb8ea02071bd8bb90f7c166fcc4ffe86
jay-inexture <jayjiyani@inexture.com>
2024-03-18 11:02:36 +0530

    dev:init

---
+++ src/main/java/com/gbs/gateway/GbsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gbs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbsGatewayApplication.class, args);
+    }
+
+}
